991708851a6cf7333035f657220dd133fa45e6b2
rollin_hand <rollin_hand>
2003-02-16 21:17:27 +0000

    protection for the whole file

--- dbf-core/endian.h
+++ dbf-core/endian.h
@@ -1,18 +1,20 @@
-/**************************************************
+/********************************************************************************
  * endian.h
- **************************************************
- * Routines for Little Endian and Big Endian
- * Systems
+ ********************************************************************************
+ * Routines for Little Endian and Big Endian Systems
  *
- * Version 0.1, 2002-12-12
+ * Version 0.2, 2003-02-16
  * Author: Björn Berg, clergyman@gmx.de
  *
  * History:
+ * 2003-02-16	jones	the #ifndef inclusion guard only protected the #defines 
+ *						where it really needs to protect the whole file. Just 
+ *						moved the #endif to the end of the file. 
  * 2003-02-09	jones	improved IsBigEndian function
  *						changes in rotate4b
  * 2003-02-01	berg	rotate2b / rotate4b added	
  * 2002-12-12	berg	first implementation
- **************************************************/	
+ *******************************************************************************/	
 
 #ifndef _ANUBISNET_ENDIAN_
 #define _ANUBISNET_ENDIAN_
@@ -23,7 +25,6 @@
 
 #define _true 0x01
 #define _false 0x00
-#endif
 
 // Routine to test if System uses Big Endian
 // or Little Endian
@@ -60,3 +61,4 @@ unsigned int rotate4b( unsigned int var) {
 	}	
 	return var;
 }
+#endif

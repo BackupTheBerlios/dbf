3f272246932a3e8f201c87e7aa0db213d9e58671
rollin_hand <rollin_hand>
2003-11-09 13:59:32 +0000

    macros to determine the used database revision

--- dbf-core/src/dbf.h
+++ dbf-core/src/dbf.h
@@ -27,11 +27,23 @@
 #include "csv.h"
 #include "sql.h"
 
+#define FoxBase 0x02			/* Code for FoxBase */
+#define FoxBasePlus 0x03		/* Code for FoxBasePlus, same as for dBase III */
+#define dBase3 0x03				/* Code for dBase III */
+#define dBase3WM 0x83			/* Code for dBase III with memo fields */
+#define dBase4 0x04				/* Code for dBase IV */
+#define dBase4WM 0x8B			/* Code for dBase IV with memo fields */
+#define dBase4SQL 0x8E			/* Code for dBase IV with SQL table */
+#define dBase5 0x05				/* Code for dBase 5.0 */
+#define FoxPro2WM 0xF5			/* Code for FoxPro 2.0 (or earlier) with memo fields */					
+#define VisualFoxPro 0x30		/* Code for Visual FoxPro without memo fields */
+
 /* 
  * V A R I A B L E S 
  */
 
-extern	unsigned int verbosity;
+extern unsigned int verbosity;
+extern unsigned int dbversion;
 
 typedef int	(*headerMethod)(FILE *output, const struct DB_FIELD * header,
     int header_length,

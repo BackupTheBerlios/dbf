d52b11467c1297d1f63a76825f05c9551d603d5b
steinm <steinm>
2004-08-30 10:23:51 +0000

    - added prototypes of new functions

--- dbf-core/src/sql.h
+++ dbf-core/src/sql.h
@@ -1,6 +1,6 @@
-/***********************************************************************************
+/*****************************************************************************
  * sql.h
- ***********************************************************************************
+ *****************************************************************************
  * conversion of dbf files to sql
  * 
  * Version 0.2, 2003-09-08
@@ -12,7 +12,7 @@
  * 2003-02-24	jones			some minor changes
  * - Version 0.1 - February 2003
  *	 first implementation in dbf.c
- ************************************************************************************/
+ ******************************************************************************/
 
 #ifndef _SQL_H_
 #define _SQL_H_
@@ -33,16 +33,18 @@
 #	include <sys\stat.h>
 #endif
 
-#include "an_string.h"
 #include "dbf.h"
-
 
 int setNoDrop (FILE *fp, P_DBF *p_dbf,
     const char *input, const char *separator);
 int setNoCreate (FILE *fp, P_DBF *p_dbf,
     const char *input, const char *separator);
 int setSQLTrim(FILE *fp, P_DBF *p_dbf,
-    const char *filename, const char *mode /* ``l''. or ``r'', or ``b'' */);
+    const char *filename, const char *mode);
+int setSQLUsecopy(FILE *fp, P_DBF *p_dbf,
+    const char *filename, const char *mode);
+int setSQLEmptyStrIsNULL(FILE *fp, P_DBF *p_dbf,
+    const char *filename, const char *mode);
 int writeSQLHeader(FILE *fp, P_DBF *p_dbf,
     const char *filename, const char *export_filename);
 int writeSQLLine(FILE *fp, P_DBF *p_dbf,

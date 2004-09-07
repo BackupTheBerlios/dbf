658a47f43b31bb4c9c9ea03087e5ef2ecfb51088
steinm <steinm>
2004-09-07 15:50:23 +0000

    - no need for endian.h, fixed some comments

--- dbf-core/src/statistic.h
+++ dbf-core/src/statistic.h
@@ -1,4 +1,4 @@
-/***********************************************************************************
+/*****************************************************************************
  * statistic.h
  * inherits the statistic functions for dBASE files
  * Author: Bjoern Berg, September 2002
@@ -6,12 +6,11 @@
  * dbf Reader and Converter for dBase III, IV, 5.0 
  *
  * see statistic.c for history and details
- ************************************************************************************/
+ ****************************************************************************/
 
 #ifndef _DBF_STATS_
 #define _DBF_STATS_
 #include "congraph.h"
-#include "endian.h"
 #include "dbf.h"
 
 static const char *get_db_version (int version);
